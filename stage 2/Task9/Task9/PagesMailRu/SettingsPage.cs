6b820c6594c8a4e5d0c0387fa0c484e6095e2100
Maxim Yurchenok <80195341+YurchenokMaxim@users.noreply.github.com>
2021-08-23 13:11:20 +0300

    Add files via upload

---
+++ stage 2/Task9/Task9/PagesMailRu/SettingsPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+
+namespace Task9
+{
+    public class SettingsPage : AbstractPage
+    {
+        private readonly static string nickNameId = "nickname";
+
+        private readonly static string saveButtonXPath = "//*[@id='root']/div/div[3]/div/div/div/form/div/div[2]/button[1]";
+
+        private IWebElement nickName;
+
+        private IWebElement saveButton;
+
+        public SettingsPage(IWebDriver driver) : base(driver)
+        {
+            nickName = driver.FindElement(By.Id(nickNameId));
+            saveButton = driver.FindElement(By.XPath(saveButtonXPath));
+        }
+
+        /// <summary>
+        /// This method change nickname.
+        /// </summary>
+        /// <param new nickname="newNickname"></param>
+        /// <returns>message about success</returns>
+        public string ChangeNickname(string newNickname)
+        {
+            if (newNickname == string.Empty)
+            {
+                logger.Error("New nickname does not exist");
+            }
+            nickName.Clear();
+            nickName.SendKeys(newNickname);
+            saveButton.Click();
+            return "Nickname successfuly changed";
+        }
+    }
+}
